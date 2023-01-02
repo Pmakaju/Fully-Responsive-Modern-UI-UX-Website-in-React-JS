7e32818d79d5ef65f84f5e595fac7c7adb8c71df
Pradeep Makaju <makaju.pradeep87@gmail.com>
2023-01-02 11:58:29 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  WhatGPT3,
+  Header,
+} from "./containers";
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Lets Get Started</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/brand/Brand.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./brand.css";
+
+const Brand = () => {
+  return;
+  <div>Brand</div>;
+};
+
+export default Brand;
