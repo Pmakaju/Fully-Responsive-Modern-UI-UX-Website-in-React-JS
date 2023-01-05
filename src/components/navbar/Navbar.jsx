f0aa66f708bcfbe17e90ed960d12ea500764b8dd
Pradeep Makaju <makaju.pradeep87@gmail.com>
2023-01-05 12:05:51 -0600

    made navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,11 +1,28 @@
-import React from 'react';
-import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import './navbar.css';
 import logo from '../../assets/logo.svg';
 
 //BEM - Block Element Modifier
+//toggleMenu = variable that going to let us know if we're currently showing the menu, the mobile menu or not
+//setToggleMenu = function that allows us to change the variable
+// meaning at the start mobile menu is false
+
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#wgpt3">What is GPT3?</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
+//Re-using this Menu two times thats what's good about react. then simply call this componet wherever we need it without having to code two times
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <div className="gpt3__navbar">
       <div className="gpt3__navbar-links">
@@ -13,16 +30,31 @@ const Navbar = () => {
           <img src={logo} alt="logo" />
         </div>
         <div className="gpt3__navbar-links_container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT3?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog"></a></p>
+          <Menu />
         </div>
       </div>
      <div className="gpt3__navbar-sign">
         <p>Sign in</p>
         <button type="button">Sign Up</button>
+     </div>
+     <div className="gpt3__navbar-menu">
+        {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {
+          toggleMenu && (
+            <div className="gpt3__navbar-menu_container scale-up-center">
+              <div className="gpt3__navbar-menu_container-links">
+                <Menu />
+                <div className="gpt3__navbar-menu_container-links-sign">
+                  <p>Sign in</p>
+                  <button type="button">Sign Up</button>
+                </div>
+              </div>
+            </div>
+          )
+        }
      </div>
     </div>
     )
