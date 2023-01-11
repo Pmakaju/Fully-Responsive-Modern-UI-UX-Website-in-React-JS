e9a9ef3ad9b4ffe59f3ffa6eaaa8fa33926631b5
Pradeep Makaju <makaju.pradeep87@gmail.com>
2023-01-11 11:14:12 -0600

    worked on Brand-section

---
+++ src/components/brand/imports.js
@@ -0,0 +1,15 @@
+import google from '../../assets/google.png';
+import slack from '../../assets/slack.png';
+import atlassian from '../../assets/atlassian.png';
+import dropbox from '../../assets/dropbox.png';
+import shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    slack,
+    atlassian,
+    dropbox,
+    shopify,
+}
+
+// The only thing it did is now we can go back to Brand.jsx and we can import all of them in one sentence.
