bfa7b386039765ba047550a84a235426ed518b6b
Pradeep Makaju <makaju.pradeep87@gmail.com>
2023-01-11 13:01:39 -0600

    worked on Feature-component

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import './feature.css';
 
-const Feature = () => {
+//Used Props 
+//rendered title and text from WhatGPT3.jsx 
+
+const Features = ({ title, text }) => {
   return (
-    <div>Feature</div>
+    <div className="gpt3__features-container__feature">
+      <div className="gpt3__features-container__feature-title">
+        <div />
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        <p>{text}</p>
+      </div>
+    </div>
     )
 };
 
-export default Feature;
+export default Features;
