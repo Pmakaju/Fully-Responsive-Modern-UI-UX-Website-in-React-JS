350e4a7608fcd4fbfcfd3ec6d9c4ff88578adbe3
Pradeep Makaju <makaju.pradeep87@gmail.com>
2023-01-19 11:14:18 -0600

    Updates

---
+++ precache-manifest.7aa6efc24351f84c81ea8d2fb04439c0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "5c1ee4620c7e24dafc5e",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/css/main.51070efc.chunk.css"
+  },
+  {
+    "revision": "5c1ee4620c7e24dafc5e",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/js/main.5c1ee462.chunk.js"
+  },
+  {
+    "revision": "4824bc4903f26c2d73f7",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/js/1.4824bc49.chunk.js"
+  },
+  {
+    "revision": "1ba8d69d254378bb9fd9",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/js/runtime~main.1ba8d69d.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "fea810ed22f6fc37fab045129a01ed07",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/blog01.fea810ed.png"
+  },
+  {
+    "revision": "e76b2c16b1e4ccf994a24eda75591951",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/blog02.e76b2c16.png"
+  },
+  {
+    "revision": "404d1a405e0cb0936f224fcd374c6cb0",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/blog03.404d1a40.png"
+  },
+  {
+    "revision": "d682b195d844afb2a4ce1e8573602221",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/blog04.d682b195.png"
+  },
+  {
+    "revision": "576efe185f7f571f39d782fde1d28fa9",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/blog05.576efe18.png"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "a183281ee68cfd2967d97b04b94da186",
+    "url": "/Fully%20Responsive%20Modern%20UI%20UX%20Website%20in%20React%20JS/index.html"
+  }
+];
